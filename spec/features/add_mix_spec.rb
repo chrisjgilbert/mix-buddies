6d1057f3a26629d779fbabc21546eaa576d97aea
Chris Gilbert <chrisgilbert4@icloud.com>
2019-02-08 19:16:04 +0000

    changed have_link to have_button to reflect view change

--- spec/features/add_mix_spec.rb
+++ spec/features/add_mix_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "user visits homepage", type: :feature do
     visit_homepage_and_add_mix_with_tags
     expect(page).to have_content "my mix"
     expect(page).to have_selector(:css, 'a[href="https://www.blah.com"]')
-    expect(page).to have_link "techno"
-    expect(page).to have_link "house"
+    expect(page).to have_button "techno"
+    expect(page).to have_button "house"
   end
 end
